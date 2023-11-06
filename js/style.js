961a8d8253becc86d261dd582a3250bc7755c2c0
olaoluwa <146534182+olaoluwajo@users.noreply.github.com>
2023-11-06 21:52:37 +0100

    first commit

---
+++ js/style.js
@@ -0,0 +1,8 @@
+const players = [{name: "OLAOLUWA" , Symbol: "X"}];
+players.push ({name: "JOHN" , symbol: "O"});
+
+const square = document.getElementsByClassName('square');
+
+square[4].innerHTML = players[0].Symbol;
+
+square[1].innerHTML = players[1].symbol;
